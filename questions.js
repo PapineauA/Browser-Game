eac7f84e43e9ca870156b91fd709ba485e7c58ac
PapineauA <papineautaidan@gmail.com>
2022-10-25 17:52:34 -0700

    Defining the questions for the trivia section

--- questions.js
+++ questions.js
@@ -1,1 +1,71 @@
-
+//Defining the trivia questions
+var triviaQuestions = [
+  {
+    question: "Who was the victim?",
+    answers: {
+      A: "Kenny Wolf",
+      B: "Eva Wolf",
+      C: "Daniel Wolf",
+      D: "Chapman Mitchell",
+      E: "Morgan Wolf",
+    },
+    correctAnswer: "A",
+  },
+  {
+    question: "Around when was the victim's time of death?",
+    answers: {
+      A: "10:00 PM",
+      B: "10:30 PM",
+      C: "11:00 PM",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question: "When was the victim stabbed by the wooden stake?",
+    answers: {
+      A: "Before death",
+      B: "After death",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question: "Who stabbed the victim with the wooden stake?",
+    answers: {
+      A: "Kenny Wolf",
+      B: "Eva Wolf",
+      C: "Daniel Wolf",
+      D: "Chapman Mitchell",
+      E: "Morgan Wolf",
+    },
+    correctAnswer: "E",
+  },
+  {
+    question: "How was the closed room created?",
+    answers: {
+      A: "The room was never a closed room to begin with",
+      B: "The victim died inside the room, and somebody went in and out of the room through the window",
+      C: "The victim died outside the room, and was carried into the room by somebody else",
+    },
+    correctAnswer: "C",
+  },
+  {
+    question: "What is the victim's cause of death?",
+    answers: {
+      A: "Blood loss",
+      B: "Poison",
+      C: "Natural causes",
+    },
+    correctAnswer: "B",
+  },
+  {
+    question: "Who is the person who killed the victim?",
+    answers: {
+      A: "Kenny Wolf",
+      B: "Eva Wolf",
+      C: "Daniel Wolf",
+      D: "Chapman Mitchell",
+      E: "Morgan Wolf",
+    },
+    correctAnswer: "C",
+  },
+];
